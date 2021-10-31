ef1a06169a7ad62ac801599fa78b2f5b50d27221
PaulinaMas <43808336+PaulinaMas@users.noreply.github.com>
2021-10-31 16:50:30 +0100

    Add files via upload

---
+++ JAProj/CppLib/resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Plik do³¹czany wygenerowany przez program Microsoft Visual C++.
+// U¿ywane przez: CppLib.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_JAPROJEKT_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_JAPROJEKT	107
+#define IDI_SMALL				108
+#define IDC_JAPROJEKT			109
+#define IDC_MYICON				2
+#define IDC_CPP				3001
+#define IDC_ASM				3002
+#define IDC_SCROLL			202
+#define ID_PLIK_OTWORZ			4000
+#define ID_ROZPOCZNIJ			4001
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Nastêpne wartoœci domyœlne dla nowych obiektów
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
---
+++ JAProj/CppLib/targetver.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// // Uwzglêdnienie elementu SDKDDKVer.h powoduje zdefiniowanie najnowszej dostêpnej platformy systemu Windows.
+// Jeœli chcesz skompilowaæ aplikacjê dla poprzedniej platformy systemu Windows, uwzglêdnij element WinSDKVer.h i
+// ustaw makro _WIN32_WINNT na platformê, która ma byæ obs³ugiwana, przed uwzglêdnieniem elementu SDKDDKVer.h.
+#include <SDKDDKVer.h>
